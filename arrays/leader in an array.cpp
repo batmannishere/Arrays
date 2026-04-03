d6636c8a1a9fbf49bb259bf1609ace84ba7aa03a
batmannishere <pulkit20.pant@gmail.com>
2026-04-03 15:41:21 +0530

    leaders in an array

---
+++ arrays/leader in an array.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+vector<int> findLeaders(vector<int> &elements, int n) {
+   int max=INT_MIN;
+   vector<int>ans;
+   for(int i=n-1;i>=0;i--){
+       if(elements[i]>max){
+           ans.push_back(elements[i]);
+           max=elements[i];
+       }
+   }
+   reverse(ans.begin(),ans.end());
+   return ans;
+}
