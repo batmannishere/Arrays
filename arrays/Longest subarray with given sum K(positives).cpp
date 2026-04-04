b24bee729ee32e19edf555f4dc55e5805ac100cc
batmannishere <pulkit20.pant@gmail.com>
2026-04-04 23:09:41 +0530

    Longest subarray with given sum K(positives)

---
+++ arrays/Longest subarray with given sum K(positives).cpp
@@ -0,0 +1,26 @@
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+     int n = a.size();
+        
+        int maxLen = 0;
+        
+        int left = 0, right = 0;
+        
+        long long sum = a[0];
+        
+        while(right < n) {
+            
+            while(left <= right && sum > k) {
+                sum -= a[left];
+                left++;
+            }
+            
+            if(sum == k) {
+                maxLen = max(maxLen, right - left + 1);
+            }
+            
+            right++;
+            if(right < n) sum += a[right];
+        }
+        
+        return maxLen;
+}
