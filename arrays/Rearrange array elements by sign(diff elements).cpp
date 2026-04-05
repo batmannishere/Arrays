a09d7e1b7d0a1bd2a4c357238c48079a4570b80a
batmannishere <pulkit20.pant@gmail.com>
2026-04-05 13:31:49 +0530

    Rearrange array elements by sign(diff elements)

---
+++ arrays/Rearrange array elements by sign(diff elements).cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> pos, neg;
+        
+        for (int x : nums) {
+            if (x > 0) pos.push_back(x);
+            else neg.push_back(x);
+        }
+
+        int i = 0, p = 0, n = 0;
+
+     (p < pos.size() && n < neg.size()) {
+            nums[i++] = pos[p++];
+            nums[i++] = neg[n++];
+        }
+
+        
+        while (p < pos.size()) {
+            nums[i++] = pos[p++];
+        }
+
+       
+        while (n < neg.size()) {
+            nums[i++] = neg[n++];
+        }
+
+        return nums;
+    }
+};
