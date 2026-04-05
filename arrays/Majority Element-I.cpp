f3506283a9141a4e0f3e39f8d14fc575aee57bdf
batmannishere <pulkit20.pant@gmail.com>
2026-04-06 00:13:44 +0530

    Majority Element-I

---
+++ arrays/Majority Element-I.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int cnt1;
+        int cnt=0;
+        int ele;
+        for(int i=0;i<n;i++){
+            if(cnt==0){
+                cnt=1;
+                ele=nums[i];
+                
+            }
+            else if(nums[i]==ele){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]==ele){
+                cnt1++;
+            }
+        }
+        if(cnt1>(n/2)){
+            return ele;
+        }
+        else{
+            return -1;
+        }
+        
+    }
+};
