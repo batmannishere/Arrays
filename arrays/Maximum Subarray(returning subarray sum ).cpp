7a3f61be07e111f7adaf67bdcaed31dd5a90850d
batmannishere <pulkit20.pant@gmail.com>
2026-04-05 18:45:51 +0530

    Maximum Subarray(returning subarray sum )

---
+++ arrays/Maximum Subarray(returning subarray sum ).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi=INT_MIN;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum=sum+nums[i];
+            if(sum<0){
+                sum=0;
+            }
+            if(sum>maxi){
+                maxi=sum;
+            }
+        }
+        return maxi;
+    }
+};
